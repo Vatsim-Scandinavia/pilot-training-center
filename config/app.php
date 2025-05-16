2cbd9881d408f8ef9744adfb512941ba2dc4d2c3
Daniel <blt950@users.noreply.github.com>
2025-05-16 23:04:58 +0200

    fix: version

--- config/app.php
+++ config/app.php
@@ -27,7 +27,7 @@ return [
     | Needs to be updated manually for each iteration we do.
     |
     */
-    'version' => '1.0.0',
+    'version' => '1.0.2',
 
     /*
     |--------------------------------------------------------------------------
