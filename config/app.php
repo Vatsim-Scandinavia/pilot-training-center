c23da65eabc2896f7fdd3edd9b8fb9198b6bd201
Sorenkai <sorenkai@sorenkai.dev>
2025-08-16 14:52:15 +0200

    fix: update version

--- config/app.php
+++ config/app.php
@@ -27,7 +27,7 @@ return [
     | Needs to be updated manually for each iteration we do.
     |
     */
-    'version' => '1.2.2',
+    'version' => '1.2.3',
 
     /*
     |--------------------------------------------------------------------------
